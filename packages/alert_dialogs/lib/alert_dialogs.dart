81b45f61422d05835bd0643cd2995dac1eba3bce
Andrea Bizzotto <bizz84@gmail.com>
2020-05-26 15:53:10 +0100

    Add `foundation.dart` as needed for kIsWeb

--- packages/alert_dialogs/lib/alert_dialogs.dart
+++ packages/alert_dialogs/lib/alert_dialogs.dart
@@ -3,6 +3,7 @@ library alert_dialogs;
 import 'dart:io';
 
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
