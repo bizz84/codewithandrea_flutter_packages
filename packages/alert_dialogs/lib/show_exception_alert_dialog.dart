44235f444f3444e41298b26e2695173e7835776d
Andrea Bizzotto <bizz84@gmail.com>
2020-08-20 11:08:53 +0100

    Update showExceptionAlertDialog

--- packages/alert_dialogs/lib/show_exception_alert_dialog.dart
+++ packages/alert_dialogs/lib/show_exception_alert_dialog.dart
@@ -14,32 +14,29 @@ Future<void> showExceptionAlertDialog({
 
 String _message(dynamic exception) {
   if (exception is FirebaseException) {
-    if (exception.message == 'FIRFirestoreErrorDomain') {
-      if (exception.code == 'Code 7') {
-        // This happens when we get a "Missing or insufficient permissions" error
-        return 'This operation could not be completed due to a server error';
-      }
-      return exception.message;
-    }
-    return _errors[exception.code] ?? exception.message;
+    return exception.message;
+  }
+  if (exception is PlatformException) {
+    return exception.message;
   }
   return exception.toString();
 }
 
+// TODO: Revisit this
 // NOTE: The full list of FirebaseAuth errors is stored here:
 // https://github.com/firebase/firebase-ios-sdk/blob/2e77efd786e4895d50c3788371ec15980c729053/Firebase/Auth/Source/FIRAuthErrorUtils.m
 // These are just the most relevant for email & password sign in:
-Map<String, String> _errors = {
-  'ERROR_WEAK_PASSWORD': 'The password must be 8 characters long or more.',
-  'ERROR_INVALID_CREDENTIAL': 'The email address is badly formatted.',
-  'ERROR_EMAIL_ALREADY_IN_USE':
-      'The email address is already registered. Sign in instead?',
-  'ERROR_INVALID_EMAIL': 'The email address is badly formatted.',
-  'ERROR_WRONG_PASSWORD': 'The password is incorrect. Please try again.',
-  'ERROR_USER_NOT_FOUND':
-      'The email address is not registered. Need an account?',
-  'ERROR_TOO_MANY_REQUESTS':
-      'We have blocked all requests from this device due to unusual activity. Try again later.',
-  'ERROR_OPERATION_NOT_ALLOWED':
-      'This sign in method is not allowed. Please contact support.',
-};
+// Map<String, String> _errors = {
+//   'ERROR_WEAK_PASSWORD': 'The password must be 8 characters long or more.',
+//   'ERROR_INVALID_CREDENTIAL': 'The email address is badly formatted.',
+//   'ERROR_EMAIL_ALREADY_IN_USE':
+//       'The email address is already registered. Sign in instead?',
+//   'ERROR_INVALID_EMAIL': 'The email address is badly formatted.',
+//   'ERROR_WRONG_PASSWORD': 'The password is incorrect. Please try again.',
+//   'ERROR_USER_NOT_FOUND':
+//       'The email address is not registered. Need an account?',
+//   'ERROR_TOO_MANY_REQUESTS':
+//       'We have blocked all requests from this device due to unusual activity. Try again later.',
+//   'ERROR_OPERATION_NOT_ALLOWED':
+//       'This sign in method is not allowed. Please contact support.',
+// };
